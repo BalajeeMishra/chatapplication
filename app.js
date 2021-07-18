50b2365c4dd134aaeb358ae4221b52e3cff70133
BalajeeMishra <73851254+BalajeeMishra@users.noreply.github.com>
2021-07-19 00:22:04 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+const io = require("socket.io")(process.env.PORT || 8000)
+const express=require("express");
+const path = require("path");
+const app=express();
+const users = {};
+io.on("connection", socket => {
+    socket.on("new-user-joined", n => {
+        console.log("new user ", n);
+        users[socket.id] = n;
+        // socket.brodcast.emit("user-joined", n);
+        io.sockets.emit("user-joined", n);
+    });
+
+    socket.on("send", message => {
+        // socket.brodcast.emit("receive", { message: message, n: users[socket.id] })
+        io.sockets.emit("receive", { message: message, n: users[socket.id] });
+    })
+    socket.on("disconnect", message => {
+        // socket.brodcast.emit("left", users[socket.id]);
+        io.sockets.emit("left", users[socket.id]);
+        delete users[socket.id];
+    })
+})
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')))
+app.get("/",(req,res)=>{
+  
+     res.render("index")
+})
+const PORT=process.env.PORT||3000
+app.listen(PORT,(req,res)=>{
+    console.log("connected");
+})
---
+++ public/client.js
@@ -0,0 +1,42 @@
+// const socket = io("http://localhost:8000");
+var socket = io('https://quiet-tor-11571.herokuapp.com', { transports: ['websocket', 'polling', 'flashsocket'] });
+const form = document.getElementById("send-container");
+const messageInput = document.getElementById("messageInp");
+const messageContainer = document.querySelector(".container");
+var audio = new Audio("ting.mp3");
+const append = (message, position) => {
+    const messageElement = document.createElement("div");
+    messageElement.innerText = message;
+    messageElement.classList.add("chat");
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+    if (position == "left") {
+        audio.play();
+    }
+}
+
+
+const n = prompt("Enter your name to join ");
+// const name = "balajee";
+socket.emit("new-user-joined", n);
+socket.on("user-joined", n => {
+    append(`${n} joined the chat`, "right");
+});
+socket.on("receive", data => {
+    append(`${data.n} : ${data.message}`, "left");
+});
+socket.on("left", n => {
+    append(`${n} left the chat`, "right");
+});
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const message = messageInput.value;
+    // const message = messageInput.innerText;
+    append(`you:${message}`, "right");
+    socket.emit("send", message);
+    messageInput.value = "";
+})
+
+
+
+
