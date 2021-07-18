1a85f689cd2ae399a0a8b670badc5d26b31b9ff1
Balajee Mishra <bala.44472@gmail.com>
2021-07-18 22:05:28 +0530

    added gor heroku deployment

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const io = require("socket.io")(8000)
+const io = require("socket.io")(process.env.PORT || 8000)
 const users = {};
 io.on("connection", socket => {
     socket.on("new-user-joined", n => {
