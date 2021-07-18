cef5ad779085cb0e1f64bcb229460f27204a7241
Balajee Mishra <bala.44472@gmail.com>
2021-07-18 21:31:00 +0530

    finally

---
+++ app.js
@@ -0,0 +1,20 @@
+const io = require("socket.io")(8000)
+const users = {};
+io.on("connection", socket => {
+    socket.on("new-user-joined", n => {
+        console.log("new user ", n);
+        users[socket.id] = n;
+        // socket.brodcast.emit("user-joined", n);
+        io.sockets.emit("user-joined", n);
+    });
+
+    socket.on("send", message => {
+        // socket.brodcast.emit("receive", { message: message, n: users[socket.id] })
+        io.sockets.emit("receive", { message: message, n: users[socket.id] });
+    })
+    socket.on("disconnect", message => {
+        // socket.brodcast.emit("left", users[socket.id]);
+        io.sockets.emit("left", users[socket.id]);
+        delete users[socket.id];
+    })
+})
